95357098e68e27428cc621b3f8fdc51790e95916
Alin Eugen Deac <aedart@gmail.com>
2017-09-05 08:46:35 +0200

    Deprecate detector, move to new namespace
    Just moved the detector and adapted it to PHP 7.1

--- src/Interfaces/IApplicationDetector.php
+++ src/Interfaces/IApplicationDetector.php
@@ -1,8 +1,10 @@
 <?php namespace Aedart\Laravel\Detector\Interfaces;
 
-use Illuminate\Contracts\Foundation\Application;
+use Aedart\Laravel\Detector\Contracts\ApplicationDetector;
 
 /**
+ * @deprecated Since version 3.0, use \Aedart\Laravel\Detector\Contracts\ApplicationDetector instead
+ *
  * Interface Application Detector
  *
  * <br />
@@ -15,38 +17,7 @@ use Illuminate\Contracts\Foundation\Application;
  * @author Alin Eugen Deac <aedart@gmail.com>
  * @package Aedart\Laravel\Detector\Interfaces
  */
-interface IApplicationDetector
+interface IApplicationDetector extends ApplicationDetector
 {
-
-    /**
-     * Check if a Laravel Application instance is available and
-     * has not been flushed
-     *
-     * @return bool True if a Laravel application is available and not yet flushed, false if
-     *              application is not available Or if it is available, but has been flushed!
-     */
-    public function isApplicationAvailable();
-
-    /**
-     * Check if there is an application instance (the container) available
-     *
-     * <b>Warning</b>: Method does NOT check if eventual application
-     * instance has been flushed.
-     *
-     * @see hasBeenFlushed()
-     * @see \Illuminate\Contracts\Container\Container
-     *
-     * @return bool True if an application instance is available, false if not
-     */
-    public function isContainerAvailable();
-
-    /**
-     * Check if the given application instance has been flushed
-     *
-     * @param Application $application The Laravel application instance
-     *
-     * @return bool True if the given application instance has been flushed, false if not
-     */
-    public function hasBeenFlushed(Application $application);
 
 }
